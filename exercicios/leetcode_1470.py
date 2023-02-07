1f92d38cd53f296ce8e2c27815e8131c021a47ec
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-02-06 22:59:17 -0300

    adiciona exercicio 1470 do leetcode

---
+++ exercicios/leetcode_1470.py
@@ -0,0 +1,18 @@
+# 1470. Shuffle the Array
+
+class Solution:
+    result = []
+    def shuffle(self, nums, n):
+        self.result = []
+        for i in range(2 * n):
+            self.result.append(0)
+
+        for i in range(n):
+            self.result[2 * i] = nums[i]
+            self.result[2 * i + 1] = nums[n + i]
+
+        return self.result
+
+
+solve = Solution()
+print(solve.shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4))
